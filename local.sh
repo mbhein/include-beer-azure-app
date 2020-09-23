b0032ad8cb8796284e14987373dc8495099f1226
mbhein <matthew.hein@gmail.com>
2020-09-22 19:03:23 -0500

    feat: correct startup cmds

--- local.sh
+++ local.sh
@@ -1,6 +1,6 @@
 # local.sh
 
-export FLASK_APP=wsgi.py
+export FLASK_APP=wsgi:app
 export FLASK_DEBUG=1
 export INCLUDE_BEER_SESSION_FILE=./data/include-beer-sessions.yml
 export INCLUDE_BEER_STATS_DIR=./data
